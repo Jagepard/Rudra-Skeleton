e30b85e452ec1ceb4330887bf066de56551d307f
Jagepard <dankorot@gmail.com>
2018-12-13 14:09:19 +0300

    Update folder

--- app/web/Controllers/MainController.php
+++ app/web/Controllers/MainController.php
@@ -6,12 +6,22 @@ use App\Web\WebController;
 
 class MainController extends WebController
 {
-
     /**
      * @Routing(url = '')
      */
     public function actionIndex()
     {
-        $this->twig('index.html.twig', (array) $this->data());
+        $this->sendMail('dankorot@gmail.com');
+        $this->twig('index', (array)$this->data());
+    }
+
+    protected function sendMail(string $to)
+    {
+        $message = (new \Swift_Message('Подтвердите почту'))
+            ->setFrom(['jagepard@yandex.ru' => 'Администратор сайта ' . $this->container()->config('url')])
+            ->setTo([$to])
+            ->setBody('Подтвердите почту');
+
+        $this->container()->get('mailer')->send($message);
     }
 }
