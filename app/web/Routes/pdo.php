be930362691b4f102ba7a9e0728f32d256343cd0
Jagepard <dankorot@gmail.com>
2018-12-17 17:03:45 +0300

    Update

--- app/web/Routes/pdo.php
+++ app/web/Routes/pdo.php
@@ -1,7 +1,5 @@
 <?php
 
 return [
-    'stargate'       => ['LoginController'],
-    'stargate::POST' => ['LoginController', 'actionLogin'],
-    'logout'         => ['LoginController', 'actionLogout']
+    'mainpage' => ['MainController'],
 ];
