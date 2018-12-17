9b68dd8e99a1f410a8946748226f466a58fe4ace
Jagepard <dankorot@gmail.com>
2018-12-17 17:04:05 +0300

    Update

--- app/web/Controllers/MainController.php
+++ app/web/Controllers/MainController.php
@@ -6,22 +6,12 @@ use App\Web\WebController;
 
 class MainController extends WebController
 {
+
     /**
      * @Routing(url = '')
      */
     public function actionIndex()
     {
-        $this->sendMail('dankorot@gmail.com');
         $this->twig('index', (array)$this->data());
-    }
-
-    protected function sendMail(string $to)
-    {
-        $message = (new \Swift_Message('Подтвердите почту'))
-            ->setFrom(['jagepard@yandex.ru' => 'Администратор сайта ' . $this->container()->config('url')])
-            ->setTo([$to])
-            ->setBody('Подтвердите почту');
-
-        $this->container()->get('mailer')->send($message);
     }
 }
