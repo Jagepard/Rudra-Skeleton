46d7ff1ea0d9463781f95354d6e940f0422ffda8
Jagepard <dankorot@gmail.com>
2019-01-16 17:33:34 +0300

    Update

--- app/services.php
+++ app/services.php
@@ -1,25 +1,25 @@
 <?php
 
-use Rudra\Container;
 use Rudra\Interfaces\ContainerInterface;
-
-$rudra = Container::$app;
 
 return [
     'contracts' => [
-        ContainerInterface::class => Container::$app,
+        ContainerInterface::class => rudra(),
+        Swift_Transport::class    => (new Swift_SmtpTransport('smtp.yandex.com', 465, 'ssl'))
+            ->setUsername(config('swiftmailer', 'username'))
+            ->setPassword(config('swiftmailer', 'password'))
     ],
 
     'services' => [
         'debugbar'         => ['DebugBar\StandardDebugBar'],
         'annotation'       => ['Rudra\Annotation'],
         'validation'       => ['Rudra\Validation'],
-        'auth'             => ['Rudra\Auth', ['env' => $rudra->config('env'), 'role' => $rudra->config('role')]],
-        'redirect'         => ['Rudra\Redirect', ['url' => APP_URL, 'env' => $rudra->config('env')]],
-        'connector'        => ['Rudra\Connector', ['config' => $rudra->config('database', $rudra->config('database', 'active'))]],
-        'router'           => ['Rudra\Router', ['namespace' => $rudra->config('namespaces', 'web')]],
+        'auth'             => ['Rudra\Auth', [config('env'), config('role')]],
+        'redirect'         => ['Rudra\Redirect', [config('url'), config('env')]],
+        'connector'        => ['Rudra\Connector', [config('database', config('database', 'active'))]],
+        'router'           => ['Rudra\Router', [config('namespaces', 'web')]],
         'route'            => ['App\Route'],
         'event.dispatcher' => ['Rudra\EventDispatcher'],
-        'mailer'           => ['\Swift_Mailer', ['transport' => $rudra->get('transport')]]
+        'mailer'           => ['\Swift_Mailer']
     ]
 ];
