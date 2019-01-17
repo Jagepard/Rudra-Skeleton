c8336ad3524f77f328c07d68962a661dabba2de0
Jagepard <dankorot@gmail.com>
2019-01-17 13:49:36 +0300

    Update

--- app/route.php
+++ app/route.php
@@ -3,22 +3,20 @@
 namespace App;
 
 use Rudra\ExternalTraits\RouteTrait;
-use Rudra\ExternalTraits\SetContainerTrait;
 
 class Route
 {
 
     use RouteTrait;
-    use SetContainerTrait;
 
     /**
      * @throws \Rudra\Exceptions\RouterException
      */
     public function run()
     {
-        $this->route('auth', config('database', 'active'));
-        $this->route('web', config('database', 'active'));
-//        $this->collect(config('namespaces'), config('database', 'active'));
+//        $this->route('auth', config('database', 'active'));
+//        $this->route('web', config('database', 'active'));
+        $this->collect(config('namespaces'), config('database', 'active'));
         $this->handleException();
     }
 }
