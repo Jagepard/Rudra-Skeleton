e8dc42386b7bdd7baad15d2ef40d6e11bd9c5a65
Jagepard <dankorot@gmail.com>
2019-01-24 16:27:36 +0300

    Update

--- app/web/WebController.php
+++ app/web/WebController.php
@@ -8,7 +8,6 @@ use App\Common\TwigFunctions;
 use App\Auth\Models\PDO\Users as PDO;
 use DebugBar\DataCollector\ConfigCollector;
 use DebugBar\DataCollector\MessagesCollector;
-use Rudra\Interfaces\ContainerInterface;
 
 class WebController extends Controller
 {
@@ -19,15 +18,14 @@ class WebController extends Controller
     public function init()
     {
         $this->template(config('template', 'web'));
-        rudra()->get('debugbar')['time']->startMeasure('Controller', 'Controller');
         $this->checkCookie();
-
         $this->setData('title', 'Rudra Framework');
         $this->setData('user', PDO::user());
     }
 
     public function twig(string $template, array $params = []): void
     {
+        rudra()->get('debugbar')['time']->startMeasure('Controller', 'Controller');
         rudra()->get('debugbar')->addCollector(new ConfigCollector($params));
         rudra()->get('debugbar')->addCollector(new MessagesCollector('Twig'));
         rudra()->get('debugbar')['Twig']->info($template);
